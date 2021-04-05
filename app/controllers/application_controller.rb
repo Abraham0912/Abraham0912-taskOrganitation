9f6ae6083628266dea56af7a40c6c3fd2df2bbcf
Abraham0912 <09120912ahn@gmail.com>
2021-04-05 18:20:40 -0500

    internacianlizacion del titulo nueva categoria con clase I18n, tieien que ver 4 archivos = new,html.haml, application_controller.rb,es.yml,es_simple_form.yml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    before_action :set_locale #Helper "before_action"es decir, antes de cualquier accion llama al metodo set_locate por medio de su simbolo
+
+    def set_locale#Esta accion o metodo indica el idioma que esta en config/locales/es.yml
+        I18n.locale = 'es'
+    end
 end
