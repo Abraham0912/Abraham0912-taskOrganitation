ae4ac9a0258807a387c2d918ea625010c0f504f2
Abraham0912 <09120912ahn@gmail.com>
2021-04-05 21:59:49 -0500

    agregando al metodo before_action un metodo de la gema devise en application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
     before_action :set_locale #Helper "before_action"es decir, antes de cualquier accion llama al metodo set_locate por medio de su simbolo
 
+    before_action :authenticate_user! #:authenticate_user! metodo de la gema devise
+
     def set_locale#Esta accion o metodo indica el idioma que esta en config/locales/es.yml
         I18n.locale = 'es'
     end
