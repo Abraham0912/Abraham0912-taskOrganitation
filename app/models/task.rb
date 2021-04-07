f2d63363b1e6d152a49a88f09ae95580f9cf590f
Abraham0912 <09120912ahn@gmail.com>
2021-04-07 11:01:30 -0500

    add model Paticipant and relacion with tables or models user.rb and task.rb, also annotate run comand annotate --models

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,11 +9,16 @@
 #  category_id :bigint
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  owner_id    :bigint           not null
 #
 class Task < ApplicationRecord
   belongs_to :category
   belongs_to :owner, class_name: 'User'#Pertenece a :owner, como esa tabla no existe hacemos referencia al modelo User
+  has_many :participating_users, class_name: 'Participant'
+  has_many :participants, through: :participating_users, source: :user #Aqui en lugar de source iria class_name, pero como se uso through usarmos resource para referirnos al modelo de :participants
   
+  validates :participating_users, presence: true
+
   validates :name, :description, presence: true#Campos requeridos name  y decription
   validates :name, uniqueness: { case_sensitive: false }#Valores unicos en :name y diferencia entre mayusculas y minusculas
   validate :due_date_validity #Validando por medio de una funcion creada por mi, para ello hacemos uso de la funcion "validate"
